cc5a4c82df47c25537ca18e2a7b29a5aae28b378
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2024-09-10 14:24:02 -0300

    feat: add postgresql integration

--- src/charm.py
+++ src/charm.py
@@ -11,6 +11,11 @@ import logging
 import typing
 
 import ops
+from charms.data_platform_libs.v0.data_interfaces import (
+    DatabaseCreatedEvent,
+    DatabaseEndpointsChangedEvent,
+    DatabaseRequires,
+)
 from ops import pebble
 
 logger = logging.getLogger(__name__)
@@ -29,11 +34,18 @@ class MaubotCharm(ops.CharmBase):
             args: Arguments passed to the CharmBase parent constructor.
         """
         super().__init__(*args)
+        self.postgresql = DatabaseRequires(
+            self, relation_name="postgresql", database_name=self.app.name
+        )
         self.framework.observe(self.on.maubot_pebble_ready, self._on_maubot_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
+        # Integrations events handlers
+        self.framework.observe(self.postgresql.on.database_created, self._on_database_created)
+        self.framework.observe(self.postgresql.on.endpoints_changed, self._on_endpoints_changed)
 
-    def _on_maubot_pebble_ready(self, _: ops.PebbleReadyEvent) -> None:
-        """Handle maubot pebble ready event."""
+    def _reconcile(self) -> None:
+        """Reconcile workload configuration."""
+        self.unit.status = ops.MaintenanceStatus()
         container = self.unit.get_container(MAUBOT_CONTAINER_NAME)
         if not container.can_connect():
             return
@@ -41,16 +53,42 @@ class MaubotCharm(ops.CharmBase):
         container.replan()
         self.unit.status = ops.ActiveStatus()
 
+    def _on_maubot_pebble_ready(self, _: ops.PebbleReadyEvent) -> None:
+        """Handle maubot pebble ready event."""
+        self._reconcile()
+
     def _on_config_changed(self, _: ops.ConfigChangedEvent) -> None:
         """Handle changed configuration."""
-        self.unit.status = ops.MaintenanceStatus()
-        container = self.unit.get_container(MAUBOT_CONTAINER_NAME)
-        if not container.can_connect():
-            return
-        container.add_layer(MAUBOT_SERVICE_NAME, self._pebble_layer, combine=True)
-        container.replan()
-        self.unit.status = ops.ActiveStatus()
+        self._reconcile()
 
+    # Integrations events handlers
+    def _on_database_created(self, _: DatabaseCreatedEvent) -> None:
+        """Handle database created event."""
+        self._reconcile()
+
+    def _on_endpoints_changed(self, _: DatabaseEndpointsChangedEvent) -> None:
+        """Handle endpoints changed event."""
+        self._reconcile()
+
+    # Relation data handlers
+    def _get_postgresql_credentials(self) -> str | None:
+        """Get postgresql credentials from the postgresql integration.
+
+        Returns:
+            postgresql credentials.
+        """
+        relation = self.model.get_relation("postgresql")
+        if not relation or not relation.app:
+            return None
+        endpoint = self.postgresql.fetch_relation_field(relation.id, "endpoints")
+        database = self.postgresql.fetch_relation_field(relation.id, "database")
+        username = self.postgresql.fetch_relation_field(relation.id, "username")
+        password = self.postgresql.fetch_relation_field(relation.id, "password")
+        if not all((endpoint, database, username, password)):
+            return None
+        return f"postgresql://{username}:{password}@{endpoint}/{database}"
+
+    # Properties
     @property
     def _pebble_layer(self) -> pebble.LayerDict:
         """Return a dictionary representing a Pebble layer."""
