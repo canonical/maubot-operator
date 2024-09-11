82ed11b09b5f896c8be4a9b9e3691d3ffb1aad7e
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2024-09-11 11:22:46 -0300

    feat: add ingress integration

--- src/charm.py
+++ src/charm.py
@@ -11,6 +11,11 @@ import logging
 import typing
 
 import ops
+from charms.traefik_k8s.v2.ingress import (
+    IngressPerAppReadyEvent,
+    IngressPerAppRequirer,
+    IngressPerAppRevokedEvent,
+)
 from ops import pebble
 
 logger = logging.getLogger(__name__)
@@ -29,8 +34,11 @@ class MaubotCharm(ops.CharmBase):
             args: Arguments passed to the CharmBase parent constructor.
         """
         super().__init__(*args)
+        self.ingress = IngressPerAppRequirer(self, port=29316)
         self.framework.observe(self.on.maubot_pebble_ready, self._on_maubot_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.ingress.on.ready, self._on_ingress_ready)
+        self.framework.observe(self.ingress.on.revoked, self._on_ingress_revoked)
 
     def _on_maubot_pebble_ready(self, _: ops.PebbleReadyEvent) -> None:
         """Handle maubot pebble ready event."""
@@ -48,6 +56,24 @@ class MaubotCharm(ops.CharmBase):
         if not container.can_connect():
             return
         container.add_layer(MAUBOT_SERVICE_NAME, self._pebble_layer, combine=True)
+        container.replan()
+        self.unit.status = ops.ActiveStatus()
+
+    def _on_ingress_ready(self, _: IngressPerAppReadyEvent) -> None:
+        """Handle ingress ready event."""
+        container = self.unit.get_container(MAUBOT_CONTAINER_NAME)
+        if not container.can_connect():
+            return
+        container.add_layer(MAUBOT_CONTAINER_NAME, self._pebble_layer, combine=True)
+        container.replan()
+        self.unit.status = ops.ActiveStatus()
+
+    def _on_ingress_revoked(self, _: IngressPerAppRevokedEvent) -> None:
+        """Handle ingress revoked event."""
+        container = self.unit.get_container(MAUBOT_CONTAINER_NAME)
+        if not container.can_connect():
+            return
+        container.add_layer(MAUBOT_CONTAINER_NAME, self._pebble_layer, combine=True)
         container.replan()
         self.unit.status = ops.ActiveStatus()
 
