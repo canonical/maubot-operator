e49a67dc92378d182c1abc9b37da819bfaa099f0
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2025-03-21 17:24:55 -0300

    Add test_matrix_auth_request_processed

--- tests/unit_ops/test_charm.py
+++ tests/unit_ops/test_charm.py
@@ -6,8 +6,11 @@
 
 """Unit tests for the Maubot module using testing."""
 
-import ops
+from unittest.mock import MagicMock
+
+from charms.synapse.v0.matrix_auth import MatrixAuthRequires
 from ops import testing
+from pytest import MonkeyPatch
 
 from charm import MaubotCharm
 
@@ -18,12 +21,14 @@ def test_config_changed_no_postgresql(base_state: dict):
     act: run config_changed.
     assert: status is blocked because there is no postgresql integration.
     """
-    state = ops.testing.State(**base_state)
-    context = ops.testing.Context(
+    state = testing.State(**base_state)
+    context = testing.Context(
         charm_type=MaubotCharm,
     )
+
     out = context.run(context.on.config_changed(), state)
-    assert out.unit_status == ops.testing.BlockedStatus("postgresql integration is required")
+
+    assert out.unit_status == testing.BlockedStatus("postgresql integration is required")
 
 
 def test_config_changed_with_postgresql(base_state: dict):
@@ -48,12 +53,14 @@ def test_config_changed_with_postgresql(base_state: dict):
         },
     )
     base_state["relations"] = [postgresql_relation]
-    state = ops.testing.State(**base_state)
-    context = ops.testing.Context(
+    state = testing.State(**base_state)
+    context = testing.Context(
         charm_type=MaubotCharm,
     )
+
     out = context.run(context.on.config_changed(), state)
-    assert out.unit_status == ops.testing.ActiveStatus()
+
+    assert out.unit_status == testing.ActiveStatus()
     container_root_fs = list(base_state["containers"])[0].get_filesystem(context)
     config_file = container_root_fs / "data" / "config.yaml"
     assert f"postgresql://{username}:{password}@{endpoints}/{database}" in config_file.read_text()
@@ -71,11 +78,45 @@ def test_postgresql_relation_departed(base_state: dict):
         remote_app_name="postgresql",
     )
     base_state["relations"] = [postgresql_relation]
-    state = ops.testing.State(**base_state)
-    context = ops.testing.Context(
+    state = testing.State(**base_state)
+    context = testing.Context(
         charm_type=MaubotCharm,
     )
 
     out = context.run(context.on.relation_departed(postgresql_relation), state)
 
-    assert out.unit_status == ops.testing.BlockedStatus("postgresql integration is required")
+    assert out.unit_status == testing.BlockedStatus("postgresql integration is required")
+
+
+def test_matrix_auth_request_processed(base_state: dict, monkeypatch: MonkeyPatch):
+    """
+    arrange: prepare maubot container.
+    act: trigger custom event matrix_auth_request_processed.
+    assert: reconcile is called.
+    """
+    endpoints = "1.2.3.4:5432"
+    username = "user"
+    password = "pass"  # nosec
+    database = "maubot"
+    postgresql_relation = testing.Relation(
+        endpoint="postgresql",
+        interface="postgresql_client",
+        remote_app_name="postgresql",
+        remote_app_data={
+            "endpoints": endpoints,
+            "username": username,
+            "password": password,
+            "database": database,
+        },
+    )
+    base_state["relations"] = [postgresql_relation]
+    state = testing.State(**base_state)
+    context = testing.Context(
+        charm_type=MaubotCharm,
+    )
+    reconcile_mock = MagicMock()
+    monkeypatch.setattr("charm.MaubotCharm._reconcile", MagicMock())
+
+    context.run(context.on.custom(MatrixAuthRequires.on.matrix_auth_request_processed), state)
+
+    reconcile_mock.assert_called_once()
